81b26a106ae8ea814ca85d6c83e086dd09d7fc95
Lev <ya.abc2001@ya.ru>
2023-10-04 15:54:37 +0500

    commitе

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { StepsManager } from './components/StepsManager/StepManager';
+
+import './styles/steps-manager.css';
+
+export function App() {
+  return (
+    <StepsManager />
+  );
+}
---
+++ src/components/StepsManager/Records/Button.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+export function Button({ buttonType, clickHandler, date }) {
+  const onClick = () => {
+    // date тут по сути вместо id
+    clickHandler(date);
+  };
+
+  return (
+    <button
+      // buttonType на случай кнопки edit
+      className={`record_${buttonType}-btn`}
+      type="button"
+      onClick={onClick}
+    >
+      {buttonType === 'remove' ? 'удалить' : 'редактировать'}
+    </button>
+  );
+}
